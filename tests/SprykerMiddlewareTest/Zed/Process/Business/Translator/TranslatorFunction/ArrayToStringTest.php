99ba1c03d94ead4525a866a455a6f7333720a971
Eugene Varava <ievgen.varava@spryker.com>
2017-12-14 10:54:36 +0100

    SM-10: initial structure of tests directories

---
+++ tests/SprykerMiddlewareTest/Zed/Process/Business/Translator/TranslatorFunction/ArrayToStringTest.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerMiddlewareTest\Zed\Process\Business\Translator\TranslatorFunction;
+
+use Codeception\Test\Unit;
+use SprykerMiddleware\Zed\Process\Business\Translator\TranslatorFunction\ArrayToString;
+
+/**
+ * Auto-generated group annotations
+ * @group SprykerMiddlewareTest
+ * @group Zed
+ * @group Process
+ * @group Business
+ * @group Translator
+ * @group TranslatorFunction
+ * @group ArrayToStringTest
+ * Add your own group annotations below this line
+ */
+class ArrayToStringTest extends Unit
+{
+    /**
+     * @return void
+     */
+    public function testArrayToString()
+    {
+        $this->assertNull(null);
+    }
+}
