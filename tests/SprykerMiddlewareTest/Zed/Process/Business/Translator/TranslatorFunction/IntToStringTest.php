5d54b45e38530b60e1d1cfdf7981dae161a03a69
Paulo Henrique Garcia <paulo.garcia@spryker.com>
2017-12-21 15:04:47 +0100

    Added Unit tests to translator functions

---
+++ tests/SprykerMiddlewareTest/Zed/Process/Business/Translator/TranslatorFunction/IntToStringTest.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerMiddlewareTest\Zed\Process\Business\Translator\TranslatorFunction;
+
+use Codeception\Test\Unit;
+use SprykerMiddleware\Zed\Process\Business\Translator\TranslatorFunction\IntToString;
+
+/**
+ * Auto-generated group annotations
+ * @group SprykerMiddlewareTest
+ * @group Zed
+ * @group Process
+ * @group Business
+ * @group Translator
+ * @group TranslatorFunction
+ * @group IntToStringTest
+ * Add your own group annotations below this line
+ */
+class IntToStringTest extends Unit
+{
+    /**
+     * @return void
+     */
+    public function testIntToString()
+    {
+        $converter = new IntToString();
+        $data = 12;
+
+        $this->assertEquals('12', $converter->translate($data));
+    }
+
+    /**
+     * @return void
+     */
+    public function testIntToStringWithFloat()
+    {
+        $converter = new IntToString();
+        $data = 12.2;
+
+        $this->assertEquals('12.2', $converter->translate($data));
+    }
+
+    /**
+     * @return void
+     */
+    public function testIntToStringWithTextString()
+    {
+        $converter = new IntToString();
+        $data = 'FooBar';
+
+        $this->assertEquals('FooBar', $converter->translate($data));
+    }
+}
---
+++ tests/SprykerMiddlewareTest/Zed/Process/Business/Translator/TranslatorFunction/StringToArrayTest.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerMiddlewareTest\Zed\Process\Business\Translator\TranslatorFunction;
+
+use Codeception\Test\Unit;
+use SprykerMiddleware\Zed\Process\Business\Translator\TranslatorFunction\StringToArray;
+
+/**
+ * Auto-generated group annotations
+ * @group SprykerMiddlewareTest
+ * @group Zed
+ * @group Process
+ * @group Business
+ * @group Translator
+ * @group TranslatorFunction
+ * @group StringToArrayTest
+ * Add your own group annotations below this line
+ */
+class StringToArrayTest extends Unit
+{
+    /**
+     * @return void
+     */
+    public function testStringToArray()
+    {
+        $converter = new StringToArray();
+        $converter->setOptions(
+            [StringToArray::OPTION_DELIMITER => '-']
+        );
+        $data = 'Foo-Bar';
+
+        $this->assertEquals(['Foo', 'Bar'], $converter->translate($data));
+    }
+
+    /**
+     * @return void
+     */
+    public function testStringToArrayWithIntegerString()
+    {
+        $converter = new StringToArray();
+        $converter->setOptions(
+            [StringToArray::OPTION_DELIMITER => '-']
+        );
+        $data = '12';
+
+        $this->assertEquals(['12'], $converter->translate($data));
+    }
+
+    /**
+     * @return void
+     */
+    public function testStringToArrayWithFloat()
+    {
+        $this->expectException('SprykerMiddleware\Zed\Process\Business\Exception\WrongTypeValueTranslatorException');
+
+        $converter = new StringToArray();
+        $converter->setOptions(
+            [StringToArray::OPTION_DELIMITER => '-']
+        );
+        $data = 12.0;
+
+        $this->assertEquals(['12'], $converter->translate($data));
+    }
+}
