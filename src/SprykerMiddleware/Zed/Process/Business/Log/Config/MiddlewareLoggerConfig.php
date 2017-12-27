d651007b0ff1302e0ee05aced8a6f0e374d609d1
Eugene Varava <ievgen.varava@spryker.com>
2017-12-27 17:08:01 +0100

    Introduced tolerable exceptions.

--- src/SprykerMiddleware/Zed/Process/Business/Log/Config/MiddlewareLoggerConfig.php
+++ src/SprykerMiddleware/Zed/Process/Business/Log/Config/MiddlewareLoggerConfig.php
@@ -67,7 +67,7 @@ class MiddlewareLoggerConfig implements LoggerConfigInterface
             'php://stderr',
             $this->loggerSettings->getVerboseLevel()
         );
-        $formatter = new LogstashFormatter(static::NAME);
+        $formatter = new LogstashFormatter($this->getChannelName());
         $streamHandler->setFormatter($formatter);
 
         return $streamHandler;
