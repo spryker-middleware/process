d199c161389ed56c27259bbef9efd499a6c3bc0a
vhrychenko <aptah199494@gmail.com>
2018-09-28 13:59:00 +0300

    Update license

--- src/SprykerMiddleware/Zed/Process/Business/Validator/ValidationRuleSet/Resolver/ValidatorPluginResolver.php
+++ src/SprykerMiddleware/Zed/Process/Business/Validator/ValidationRuleSet/Resolver/ValidatorPluginResolver.php
@@ -11,8 +11,8 @@ use SprykerMiddleware\Zed\Process\Business\Exception\MissingValidatorPluginExcep
 use SprykerMiddleware\Zed\Process\Dependency\Plugin\Validator\ValidatorPluginInterface;
 
 /**
- * MIT License
- * For full license information, please view the LICENSE file that was distributed with this source code.
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
  */
 
 class ValidatorPluginResolver implements ValidatorPluginResolverInterface
